864e4e4ec4a34f0dd7b433d6b5dabfa44fe13f66
VKSDS007 <115104610+VKSDS007@users.noreply.github.com>
2022-10-05 23:33:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
 import streamlit
 import pandas
+streamlit.title('My Moms new Healthy Dinner')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')                
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.multiselect("Pick Some Fruits:" list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
