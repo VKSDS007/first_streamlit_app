1bcf61f4de020e3641c6b92ab0519f0529a1cbd5
VKSDS007 <115104610+VKSDS007@users.noreply.github.com>
2022-10-05 23:18:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My parent new healthy dinner')
-streamlit.Header('Breakfast Menu')
-streamlit.title('Brown Braed')
-streamlit.title('Boiled Egg')
-streamlit.title('Tea')
-streamlit.title('Coffee')
+streamlit.header('Breakfast Menu')
+streamlit.text('Brown Braed')
+streamlit.text('Boiled Egg')
+streamlit.text('Tea')
+streamlit.text('Coffee')
